b5b336e27bd71e0ec70f567f34506c53568ef138
苏云龙 <suyunlong@didiglobal.com>
2021-08-03 18:35:06 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ git commit -m 'deploy'
 git push origin dev
 rm -rf dist
 npm run build
-git checkout main
+git checkout master
 rm -rf `ls  |egrep -v '(dist|node_modules)'`   
 mv dist/* ./
 git add .
 git commit -m 'publish ++'
-git push origin main
+git push origin master
 git checkout dev
